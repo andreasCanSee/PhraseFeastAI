5006b2992fdfaa16bd7de8c2ee7eb683f41cf19f
andreas <andreas@4e842236-cb89-4cd8-bbac-7553c2091f32.fritz.box>
2023-12-10 15:33:13 +0100

    Start to implement basic design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,26 +9,33 @@
 
 </script>
 
-<Banner/>
+<!--Banner/-->
 
-<main>
+<div class="flex justify-center items-center bg-orange-300 text-mainBrown p-6 shadow-xl">
+    <h1 class="text-6xl font-bold">PhraseFeast</h1>
+    <img src="/icon.png" alt="" class="w-24 h-auto ml-4">
+</div>
 
-<div class="main-container">
-    <h3>Choose the language you want to learn</h3>
-    <label>
-        <input type="radio" bind:group={$selectedLanguage} value="spanish">
-        Spanish
-    </label>
-    <label>
-        <input type="radio" bind:group={$selectedLanguage} value="german">
-        German
-    </label>
-    <label>
-        <input type="radio" bind:group={$selectedLanguage} value="french">
-        French
-    </label>
+<main class="flex justify-center items-center h-full">
+
+<div class="bg-mainYellow p-14 max-w-4xl mx-auto shadow-2xl rounded-xl">
+    <h3 class="text-2xl mb-6 text-mainBrown">Choose the language you want to learn 💬</h3>
+    <div class="flex flex-col gap-y-4 border text-mainBrown">
+        <label>
+            <input type="radio" bind:group={$selectedLanguage} value="spanish">
+            Spanish 🇪🇸
+        </label>
+        <label>
+            <input type="radio" bind:group={$selectedLanguage} value="french">
+            French 🇫🇷
+        </label>
+        <label>
+            <input type="radio" bind:group={$selectedLanguage} value="german">
+            German 🇩🇪
+        </label>
+    </div>
     
-    <button on:click={start} disabled={$selectedLanguage === ''}>Start</button>
+    <button class="" on:click={start} disabled={$selectedLanguage === ''}>Start</button>
 </div>
 
 </main>
